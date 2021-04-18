610c9b825e515e1db50f2c602a0d44c954fab4e6
Adonis A. Fernandez Reyes <adonisfr@nauta.cu>
2021-04-17 21:32:03 -0400

    Updates

---
+++ precache-manifest.45c39cfa1a4bd1d459a92c13f15723a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0d40e52fb54b99f985576931bd4339",
+    "url": "/juego-ahorcado/index.html"
+  },
+  {
+    "revision": "6dee9ca157d188dbfc7d",
+    "url": "/juego-ahorcado/static/css/2.819fa2c3.chunk.css"
+  },
+  {
+    "revision": "ff23ffd2ac542a6a34c8",
+    "url": "/juego-ahorcado/static/css/main.876438b5.chunk.css"
+  },
+  {
+    "revision": "6dee9ca157d188dbfc7d",
+    "url": "/juego-ahorcado/static/js/2.e050dd23.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/juego-ahorcado/static/js/2.e050dd23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff23ffd2ac542a6a34c8",
+    "url": "/juego-ahorcado/static/js/main.1627b337.chunk.js"
+  },
+  {
+    "revision": "0dec136581a5221f8df1",
+    "url": "/juego-ahorcado/static/js/runtime-main.b57a0eab.js"
+  },
+  {
+    "revision": "831f1a7c52df71a5ee1a8a204aa3af3b",
+    "url": "/juego-ahorcado/static/media/horca1.831f1a7c.svg"
+  },
+  {
+    "revision": "a42aa5c75b6287fbe2b596483bdcd1cd",
+    "url": "/juego-ahorcado/static/media/noise.a42aa5c7.png"
+  }
+]);
